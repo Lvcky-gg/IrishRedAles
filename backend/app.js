2fb279216d905d2589192d6735f0308dd46a052c
John O'Donnell <johnodonnell1997@icloud.com>
2022-12-06 22:43:23 -0600

    error handlers defined

--- backend/app.js
+++ backend/app.js
@@ -39,9 +39,38 @@ if (!isProduction) {
     })
   );
 
-  // backend/app.js
 const routes = require('./routes');
 app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+const { ValidationError } = require('sequelize');
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
 
 module.exports = app;
