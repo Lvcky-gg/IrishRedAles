5c3d1896b0e2e11f9c0c61e71f8e80096f11a832
John O'Donnell <johnodonnell1997@icloud.com>
2022-12-07 21:19:58 -0600

    Add Users table migration and seeds

---
+++ backend/db/migrations/20221208030848-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        type: Sequelize.STRING(256)
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
