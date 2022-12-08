7b9c4c4a6a790987853730a0d269f7aaa1bf0a08
John O'Donnell <johnodonnell1997@icloud.com>
2022-12-07 21:44:02 -0600

    authentication progress

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,40 @@
 const router = require('express').Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+router.use(restoreUser);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
+
   
 
 module.exports = router;
