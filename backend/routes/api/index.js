c9d67aa0ecbf7dd36679c5f6f86ee71f961228b3
John O'Donnell <johnodonnell1997@icloud.com>
2022-12-08 21:06:12 -0600

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+
 
 // router.get(
 //   '/restore-user',
@@ -28,10 +42,6 @@ router.use(restoreUser);
 //     });
 //   setTokenCookie(res, user);
 //   return res.json({ user });
-// });
-
-// router.post('/test', function(req, res) {
-//   res.json({ requestBody: req.body });
 // });
 
 
