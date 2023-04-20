8e4d9152646957849cd29b6182e23b4d3ecafda0
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-20 14:28:08 -0500

    Likes are done

--- backend/routes/api/breweryLikes.js
+++ backend/routes/api/breweryLikes.js
@@ -4,12 +4,35 @@ const { BreweryLike, Brewery } = require("../../db/models");
 const { requireAuth } = require("../../utils/auth.js");
 
 
-router.delete(':/breweryId/:likeId', requireAuth,async(req, res)=> {
+router.delete('/:breweryId/:likeId', requireAuth,async(req, res)=> {
+    const userId = req.user.id;
+    const { breweryId, likeId } = req.params;
+    const brewery = await Brewery.findOne({where:{Id:+breweryId}})
+    const like = await BreweryLike.findOne({where:{id:+likeId}})
+    if(brewery){
+        if(like){
+            if(userId === like.userId){
+                like.destroy();
+                res.status(200);
+                return res.json({"Message":"successfully Deleted"})
+            }else{
+                console.log(like.userId)
+                res.status(403)
+                res.json({"Message":"This like does not belong to you"});
+            }
+        }else{
+            res.status(404)
+            res.json({"Message":"This like does not exist"});
+        }
+
+    }else{
+        res.status(404)
+        res.json({"Message":"This brewery does not exist"});
+    }
 
 })
 router.get('/:breweryId', async(req, res)=> {
     const {breweryId} = req.params
-    console.log(breweryId)
     const breweryLikes = await BreweryLike.findAll({where:{breweryId:+breweryId}})
     if(breweryLikes.length){
         res.status(200);
