544789c81c9f791a6649a881accef9b179e3a98e
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-20 12:18:27 -0500

    added route

--- frontend/src/components/Loader/index.js
+++ frontend/src/components/Loader/index.js
@@ -1,22 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import './Loader.css';
+import "./Loader.css";
 
 const Loader = () => {
-    const loading = useSelector((state) => state.loading.loading);
+  const loading = useSelector((state) => state.loading.loading);
 
-    if (loading)
-        return (
-            <div className="loader">
-                <div className="loader-container">
-                    <div className="loader-spinner">
-                        <span className="spinner"></span>
-                    </div>
-                </div>
-            </div>
-        );
+  if (loading)
+    return (
+      <div className="loader">
+        <div className="loader-container">
+          <div className="loader-spinner">
+            <span className="spinner"></span>
+          </div>
+        </div>
+      </div>
+    );
 
-    return null;
+  return null;
 };
 
 export default Loader;
