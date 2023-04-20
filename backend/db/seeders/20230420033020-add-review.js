af057b62cb5da89264f707e01a4241a5720a0088
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-19 22:34:38 -0500

    development commit

--- backend/db/seeders/20230420033020-add-review.js
+++ backend/db/seeders/20230420033020-add-review.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 module.exports = {
   async up (queryInterface, Sequelize) {
-    options.tableName = "Breweries"
+    options.tableName = "Reviews"
 
       await queryInterface.bulkInsert(options, [
         {
