e015613908a230515bb284790ef3032403e3e40c
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-20 13:46:32 -0500

    added seed data

--- backend/db/seeders/20230420183555-reviewLikes.js
+++ backend/db/seeders/20230420183555-reviewLikes.js
@@ -1,25 +1,42 @@
 'use strict';
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    options.tableName = "ReviewLikes"
+ 
+     await queryInterface.bulkInsert(options, [{
+      "userId":1,
+      "reviewId":1
+      },{
+        "userId":2,
+        "reviewId":2,
+      }, 
+      {
+        "userId":1,
+        "reviewId":1,
+      },
+      {
+        "userId":1,
+        "reviewId":3,
+      }
+    ], {});
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = "ReviewLikes"
+    const Op = Sequelize.Op;
+ 
+     await queryInterface.bulkDelete(options, {
+      reviewId:{[Op.in]:[1,2,3]}
+
+     });
+
   }
 };
--- backend/db/seeders/20230420183604-breweryLikes.js
+++ backend/db/seeders/20230420183604-breweryLikes.js
@@ -1,25 +1,39 @@
 'use strict';
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    options.tableName = "BreweryLikes"
+    await queryInterface.bulkInsert(options, [{
+      "userId":1,
+      "breweryId":1
+      },{
+        "userId":2,
+        "breweryId":2,
+      }, 
+      {
+        "userId":1,
+        "breweryId":1,
+      },
+      {
+        "userId":1,
+        "breweryId":3,
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = "BreweryLikes"
+    const Op = Sequelize.Op;
+ 
+      await queryInterface.bulkDelete(options,  {
+        breweryId:{[Op.in]:[1,2,3]}
+      });
+     
   }
 };
