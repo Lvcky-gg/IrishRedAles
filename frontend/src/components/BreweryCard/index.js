8cbc1a2df6f5015ce4bfe06fdbd4417652579437
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-21 00:37:59 -0500

    css

--- frontend/src/components/BreweryCard/index.js
+++ frontend/src/components/BreweryCard/index.js
@@ -4,18 +4,14 @@ import './breweryCard.css'
 const BreweryCard = ({id, breweryName, city, state}) => {
     
     return (
-        <div >
+
             <div className="breweryCard" loading="lazy">
                 <div className="breweryCardInner">
                 <h1>{breweryName}</h1>
                 <h2>{city}, {state}</h2>
                     
                 </div>
-            </div>
-            <div className="bg-blur"></div>
-    
-        </div>
-      
+            </div>   
     )
 };
 export default BreweryCard;
