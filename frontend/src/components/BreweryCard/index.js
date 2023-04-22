b679a2b5bce248a03533a5c04834ff04ebaf7e38
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-22 16:16:13 -0500

    some of component routing

--- frontend/src/components/BreweryCard/index.js
+++ frontend/src/components/BreweryCard/index.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './breweryCard.css'
 
 const BreweryCard = ({id, breweryName, city, state}) => {
+    const navigate = useNavigate()
     
     return (
 
-            <div className="breweryCard" loading="lazy">
+            <div 
+            onClick={(e)=>{
+                e.preventDefault()
+                navigate(`/breweries/${id}`)
+            }}
+            className="breweryCard" loading="lazy">
                 <div className="breweryCardInner">
-                <h1>{breweryName}</h1>
+                <h1
+                >{breweryName}</h1>
                 <h2>{city}, {state}</h2>
                     
                 </div>
