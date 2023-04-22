db86eeb949edaddb799a15682b7c12680e6bc44d
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-22 16:02:32 -0500

    component

--- frontend/src/components/SpecificBreweryPage/index.js
+++ frontend/src/components/SpecificBreweryPage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getAllBreweries } from "../../store/breweries";
+import RatingDisplay from "../RatingDisplay";
 import './specificBrew.css'
 
 
@@ -19,11 +20,35 @@ export const SpecificBrewery = () => {
         <div>
             {
                 brewery?(
-                <div>
+                <div className="specificBreweryContainer">
+                    <img  className="specificBreweryContainerIMG" src='https://imgs.search.brave.com/xjs25IGx1dhPbD6ueLPad87O61xUBgTRbd8qeIHCFwQ/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5N/aUZzel90dlB5R0Vw/VXpoSDFONkVRSGFG/aiZwaWQ9QXBp' />
+                    <div className="specificBreweryContainerBtn">
+                        <button>Add review</button>
+                        <button>placeholder</button>
+                        <button>placeholder</button>
+                    </div>
+                    <div className="specificBreweryContainerimgs">
+                        images placeholder
+                    </div>
+                    <div className="specificBreweryContainerinfo">
+                        <h1>{brewery.breweryName}</h1>
+                        <h2>Location</h2>
+                        <p>{brewery.addressLineOne}, {brewery.city}</p>
+                        <p>{brewery.state} </p>
+                        <h2>About</h2>
+                        <p>{brewery.description}</p>
+                        <RatingDisplay 
+                        rating={brewery.rating}
+                        ></RatingDisplay>
+                    </div>
+                    <div>
+                        ratings placeholder
+                    </div>
+
                     
                 </div>):(
                 <div>
-                       
+                     //this key is empty going to be a redirect  
                 </div>)
             }
 
