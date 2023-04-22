8aed3ffe6b3eff63bc0c4c56835593317452471f
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-22 15:21:45 -0500

    added source for component

--- frontend/src/components/SpecificBreweryPage/index.js
+++ frontend/src/components/SpecificBreweryPage/index.js
@@ -1,19 +1,31 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getAllBreweries } from "../../store/breweries";
 import './specificBrew.css'
 
 
-export const SpecificBrewery = ({
-    id, breweryName, ownerId, addressLineOne, city, zip, state, country, lat, lng, rating, createdAt,
-    updatedAt, reviews
-}) => {
+export const SpecificBrewery = () => {
     const {breweryId } = useParams();
+    const dispatch = useDispatch()
     const breweries = useSelector((state)=> state.breweries.allBreweries)
+    const brewery = breweries[+breweryId -1]
+    useEffect(()=>{
+        dispatch(getAllBreweries())
+    },[dispatch])
     
     console.log(breweryId);
     return (
         <div>
+            {
+                brewery?(
+                <div>
+                    
+                </div>):(
+                <div>
+                       
+                </div>)
+            }
 
         </div>
     )
