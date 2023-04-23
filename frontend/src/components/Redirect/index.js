6fef42430d5341554de1854595b8dfc1a82ebd93
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-22 20:58:20 -0500

    no change

--- frontend/src/components/Redirect/index.js
+++ frontend/src/components/Redirect/index.js
@@ -3,27 +3,31 @@ import { useNavigate } from 'react-router-dom';
 import logo from '../../images/086f9e39-3d3b-431d-b928-a129c3901f2d-profile_image-300x300.png'
 
 import './Redirect.css';
-import { useSelector } from 'react-redux';
+
 
 const Redirect = () => {
     const [redirectTimer, setRedirectTimer] = useState(5);
     const navigate = useNavigate();
     const message = "This keg is empty, returning home."
+ 
 
     useEffect(()=>{
         const startTime = Date.now();
+        
 
         const redirectInterval = setInterval(() => {
             setRedirectTimer(Math.ceil((startTime + 5000 - Date.now()) / 1000));
         }, 1000);
         const redirectTimeout = setTimeout(() => {
-            navigate('/');
+            // navigate('/');
+
         }, 5000);
         return () => {
             clearInterval(redirectInterval);
             clearTimeout(redirectTimeout);
         };
     },[]);
+
 
     return (
         <div className='redirect'>
