284cd856e0f4751c920e9f12166acee72976871f
John O'Donnell <johnodonnell1997@icloud.com>
2023-04-24 19:17:54 -0500

    signup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,179 +1,163 @@
-import { csrfFetch } from './csrf';
+import { csrfFetch } from "./csrf";
 
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios'
-import Cookies from 'js-cookie';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export const sessionSlice = createSlice({
-    name: 'session',
-    initialState: {
-        user: null,
-        error: null,
-        validationErrors: null,
-        redirectMessage: null,
+  name: "session",
+  initialState: {
+    user: null,
+    error: null,
+    validationErrors: null,
+    redirectMessage: null,
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
     },
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload;
-        },
-        removeUser: (state) => {
-            state.user = null;
-        },
-        clearErrors: (state) => {
-            state.error = null;
-            state.validationErrors = null;
-        },
-        setRedirectMessage: (state, action) => {
-            state.redirectMessage = action.payload;
-        },
+    removeUser: (state) => {
+      state.user = null;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(authenticate.fulfilled, (state, action) => {
-                state.user = action.payload;
-                state.error = null;
-            })
-            .addCase(login.fulfilled, (state, action) => {
-                state.user = action.payload;
-                state.error = null;
-                state.validationErrors = null;
-            })
-            .addCase(login.rejected, (state, action) => {
-                console.log(action)
-                state.error = action.payload.errors;
-                state.validationErrors = action.payload.errors;
-                state.user = null;
-            })
-            .addCase(logout.fulfilled, (state) => {
-                state.user = null;
-            })
-            .addCase(signUp.pending, (state) => {
-                state.error = null;
-                state.validationErrors = null;
-            })
-            .addCase(signUp.fulfilled, (state, action) => {
-                state.user = action.payload;
-                state.error = null;
-                state.validationErrors = null;
-            })
-            .addCase(signUp.rejected, (state, action) => {
-                state.error = action.payload.message;
-                state.validationErrors = action.payload.errors;
-                state.user = null;
-            });
+    clearErrors: (state) => {
+      state.error = null;
+      state.validationErrors = null;
     },
+    setRedirectMessage: (state, action) => {
+      state.redirectMessage = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(authenticate.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.error = null;
+        state.validationErrors = null;
+      })
+      .addCase(login.rejected, (state, action) => {
+        console.log(action);
+        state.error = action.payload.errors;
+        state.validationErrors = action.payload.errors;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+      })
+      .addCase(signUp.pending, (state) => {
+        state.error = null;
+        state.validationErrors = null;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.error = null;
+        state.validationErrors = null;
+      })
+      .addCase(signUp.rejected, (state, action) => {
+        state.error = action.payload.message;
+        state.validationErrors = action.payload.errors;
+        state.user = null;
+      });
+  },
 });
 
 export const authenticate = createAsyncThunk(
-    'session/authenticate',
-    async () => {
-        const response = await csrfFetch('/api/session', {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-        });
-        if (response.ok) {
-            const data = await response.json();
+  "session/authenticate",
+  async () => {
+    const response = await csrfFetch("/api/session", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    if (response.ok) {
+      const data = await response.json();
 
-            if (data.errors) {
-                return;
-            }
+      if (data.errors) {
+        return;
+      }
 
-            return data.user;
-        }
+      return data.user;
     }
+  }
 );
 
 export const login = createAsyncThunk(
-    'session/login',
-    async ({ credential, password }, { rejectWithValue }) => {
-        try {
-            // const response = await csrfFetch('/api/session', {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     },
-            //     body: JSON.stringify({
-            //         credential,
-            //         password
-            //     }),
-            // });
-            const response = await axios.post(
-                '/api/session',
-                JSON.stringify({
-                            credential,
-                            password
-                        }),
-            {headers:{
-                'Content-Type': 'application/json',
-                'XSRF-Token':Cookies.get('XSRF-TOKEN')
-                }
-            }
-            )
-            .then((res)=>res)
-            // .catch((e)=>console.log(e))
-            // const data = await response.json();
-            
-         
-            // return
-            if(response.data.user)return response.data.user;
-                 } catch (error) {
-                    console.log(error.response.data.errors)
-                    
-                    // message: "The provided credentials were invalid."
-                 return rejectWithValue({errors:error.response.data.errors});
-              }
-   
+  "session/login",
+  async ({ credential, password }, { rejectWithValue }) => {
+    try {
+      const response = await axios
+        .post(
+          "/api/session",
+          JSON.stringify({
+            credential,
+            password,
+          }),
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "XSRF-Token": Cookies.get("XSRF-TOKEN"),
+            },
+          }
+        )
+        .then((res) => res);
+
+      if (response.data.user) return response.data.user;
+    } catch (error) {
+      return rejectWithValue({ errors: error.response.data.errors });
     }
+  }
 );
 
-export const logout = createAsyncThunk('session/logout', async () => {
-    const response = await csrfFetch('/api/session', {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+export const logout = createAsyncThunk("session/logout", async () => {
+  const response = await csrfFetch("/api/session", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-        return true;
-    }
+  if (response.ok) {
+    return true;
+  }
 });
 
 export const signUp = createAsyncThunk(
-    'session/signUp',
-    async ({ firstName, lastName, username, email, password }, { rejectWithValue }) => {
-        try {
-            const response = await csrfFetch('/api/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    firstName,
-                    lastName,
-                    username,
-                    email,
-                    password,
-                }),
-            });
-
-            const data = await response.json();
-
-            if (!response.ok) {
-                if (data.errors) {
-                    return rejectWithValue({ errors: data.errors });
-                }
+  "session/signUp",
+  async (
+    { firstName, lastName, username, email, password },
+    { rejectWithValue }
+  ) => {
+    try {
+        const response = await axios
+          .post(
+            "/api/users",
+            JSON.stringify({
+            firstName,
+            lastName,
+            username,
+            email,
+            password,
+            }),
+            {
+              headers: {
+                "Content-Type": "application/json",
+                "XSRF-Token": Cookies.get("XSRF-TOKEN"),
+              },
             }
-
-            return data.user;
-        } catch (error) {
-            return rejectWithValue({ message: error.message });
-        }
-    }
+          )
+          .then((res) => res);
+  
+        if (response.data.user) return response.data.user;
+      } catch (error) {
+        return rejectWithValue({ errors: error.response.data.errors });
+      }
+  }
 );
 
 export const { setUser, removeUser, clearErrors, setRedirectMessage } =
-    sessionSlice.actions;
+  sessionSlice.actions;
 
 export default sessionSlice.reducer;
