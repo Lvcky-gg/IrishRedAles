34fb3b06322bd00758d99f4b589f59634612ed5b
John O'Donnell <johnodonnell1997@icloud.com>
2023-05-01 23:16:57 -0500

    development commit

--- backend/routes/api/photos.js
+++ backend/routes/api/photos.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { requireAuth } = require("../../utils/auth.js");
+const { Photo } = require("../../db/models");
+const { singleMulterUpload, singlePublicFileUpload } = require("../../awsS3.js");
+
+
+router.post(
+    "/:breweryId",
+    singleMulterUpload("image"),
+    requireAuth,
+    async(req, res)=>{
+        const {breweryId} = req.params;
+        const userId = req.user.id;
+        const imgUpload = await singlePublicFileUpload(req.file)
+        const img = await Photo.create({
+            breweryId:+breweryId,
+            userId:+userId,
+            URL:imgUpload
+        })
+        res.json(img)
+    }
+)
 
 
 module.exports = router
