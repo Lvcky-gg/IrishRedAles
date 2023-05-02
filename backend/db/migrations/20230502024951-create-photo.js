aba967d768d216ac230b598853425d82ffaa663f
John O'Donnell <johnodonnell1997@icloud.com>
2023-05-02 15:50:15 -0500

    errorDeployment

--- backend/db/migrations/20230502024951-create-photo.js
+++ backend/db/migrations/20230502024951-create-photo.js
@@ -1,8 +1,13 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Photos', {
+    options.tableName = 'Photos'
+    await queryInterface.createTable(options, {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -33,6 +38,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Photos');
+    options.tableName = 'Photos'
+    await queryInterface.dropTable(options);
   }
 };
